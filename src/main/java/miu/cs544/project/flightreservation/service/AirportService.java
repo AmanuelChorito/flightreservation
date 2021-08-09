bdaa8228e829445ba1233f0422b49981265d3fc3
YitbeG2770 <yitbarekgulilat@gmail.com>
2021-08-09 16:32:08 -0500

    this commit is fore airport service implementation

--- src/main/java/miu/cs544/project/flightreservation/service/AirportService.java
+++ src/main/java/miu/cs544/project/flightreservation/service/AirportService.java
@@ -1,6 +1,7 @@
 package miu.cs544.project.flightreservation.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,18 +10,15 @@ import lombok.Data;
 import miu.cs544.project.flightreservation.model.Airport;
 import miu.cs544.project.flightreservation.repository.AirPortRepository;
 
-@Service
-@Data
-public class AirportService {
-
-	@Autowired
-	private AirPortRepository repository;
+public interface AirportService {
 	
-	public List<Airport> allAirports(){
-		return repository.findAll();
-	}
+	public List<Airport> allAirports();
 	
-	public Airport saveAirport(Airport airport) {
-		return repository.save(airport);
-	}
+	public Optional<Airport> oneAirport(int id);
+	
+	public void deleteAirport(int id);
+	
+	public Airport saveAirport(Airport airport);
+	
+	public Airport updateAirport(int id, Airport airport);
 }
